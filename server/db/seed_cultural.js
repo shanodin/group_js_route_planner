40cd67359cee7c8aa0e6ece90ed7bb54d02bb8e0
Benjamin Conway <benrconway84@gmail.com>
2017-10-27 11:01:26 +0100

    Scripts included in package.json

--- server/db/seed_cultural.js
+++ server/db/seed_cultural.js
@@ -1,9 +1,9 @@
-use ;
+use route_planner;
 
 db.cultural.drop();
 
 db.cultural.insert([
   {
-
+    name: "Edinburgh Castle"
   }
 ])
--- server/db/seed_routes.js
+++ server/db/seed_routes.js
@@ -1,9 +1,9 @@
-use  ;
+use  route_planner;
 
 db.routes.drop()
 
 db.routes.insert([
   {
-    
+    name: "3 hour route"
   }
 ])
