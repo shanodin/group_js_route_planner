6e94c6ffc007479f51295c871238b8366fde494a
Andrew Samtoy <asamtoy@gmail.com>
2017-10-27 14:39:04 +0100

    Added first route

--- client/app.js
+++ client/app.js
@@ -2,12 +2,26 @@ var MapWrapper = require('./views/mapWrapper.js')
 
 var app = function () {
   renderMap()
-  
 }
 
 var renderMap = function () {
   var mapDiv = document.querySelector('div#main-map')
   var mainMap = new MapWrapper(mapDiv)
+  directionsService = new google.maps.DirectionsService(),
+  directionsDisplay = new google.maps.DirectionsRenderer({
+    map: mainMap.googleMap
+  })
+
+  var codeclan = new google.maps.LatLng(55.9445594, -3.1984787);
+  var lalba = new google.maps.LatLng(55.9604718, -3.2035689);
+
+  directionsService.route({
+    origin: codeclan,
+    destination: lalba,
+    travelMode: google.maps.TravelMode.WALKING
+  }, function(response, status){
+    directionsDisplay.setDirections(response)
+  })
 }
 
 window.addEventListener("DOMContentLoaded", app);
