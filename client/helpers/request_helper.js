cffe99877634facf522a6bb20da32405cfb010db
Alice Rees <alice.rees90@gmail.com>
2017-10-28 10:36:02 +0100

    add get and post to request helper

--- client/helpers/request_helper.js
+++ client/helpers/request_helper.js
@@ -1,4 +1,25 @@
-var requestHelper = {}
+var requestHelper = {
+  getRequest: function (url, callback) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
 
+    xhr.addEventListener('load', function () {
+      var jsonString = xhr.responseText
+      var data = JSON.parseO(jsonString)
+      callback(data)
+    })
+    xhr.send()
+  },
 
-module.exports = requestHelper;
+  postRequest: function (url, callback, payload) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('POST', url)
+
+    xhr.setRequestHeader('Content-Type', 'application/json')
+
+    var jsonString = JSON.stringify(payload)
+    xhr.send(jsonString)
+  }
+}
+
+module.exports = requestHelper
